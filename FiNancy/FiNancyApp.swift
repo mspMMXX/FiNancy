7093481c279b1bccbb2c68e47266f1cb123b5ed0
Markus <81870702+mspMMXX@users.noreply.github.com>
2024-02-01 09:17:07 +0100

    Initial Commit

---
+++ FiNancy/FiNancyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiNancyApp.swift
+//  FiNancy
+//
+//  Created by Markus Platter on 01.02.24.
+//
+
+import SwiftUI
+
+@main
+struct FiNancyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
